c6d4e675ed7818796791beba5d5ab243505bcaad
Michael <wtfdic@hotmail.com>
2020-11-19 19:31:44 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,11 +14,11 @@
 def speak_to_grandma(phrase)
   if phrase == "i love you grandma!"
     puts "HUH?! SPEAK UP, SONNY!"
-  end
 elsif phrase == "I LOVE YOU GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN"
 else
   puts "NO, NOT SINCE 1938!"
+end
 
 
 end
